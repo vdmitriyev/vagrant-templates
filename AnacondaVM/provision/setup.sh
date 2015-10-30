1d06c50d3c9730bf6e4cfa995531838dfddfe960
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2015-10-31 00:26:15 +0100

    Vagrant file for installing Anaconda and running Jupyter automatically/

--- AnacondaVM/provision/setup.sh
+++ AnacondaVM/provision/setup.sh
@@ -2,26 +2,56 @@
 echo "Provisioning virtual machine..."
 
 # Anaconda
-echo "Installing anaconda"
+
+echo "Trying to install Anaconda"
 anaconda=Anaconda-2.3.0-Linux-x86_64.sh
 
 if [ ! -f "$anaconda" ]; then
     echo "Downloading Anaconda installer. This may take more than a few minutes."
 	wget -q -o /dev/null - https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 else
-	echo "Already downloaded."
+	echo "Anaconda is already downloaded."
 fi
 
 if [ -f "$anaconda" ];  then
-
-	chmod +x "$anaconda"
-	./"$anaconda" -b -p /opt/anaconda
-	
-	# For anaconda
-	PATH=/opt/anaconda/bin:$PATH
-	echo "Anaconda installed and path set."
+	if [ ! -d "/opt/anaconda/" ]; then
+		chmod +x "$anaconda"
+		./"$anaconda" -b -p /opt/anaconda
+		
+		# For anaconda
+		PATH=/opt/anaconda/bin:$PATH
+		echo "Anaconda was installed."
+		
+		echo "PATH=/opt/anaconda/bin:\$PATH" >> /home/vagrant/.bashrc
+		echo "PATH was set in .bashrc"
+		conda create --name analysis python=2
+	else
+		# For anaconda
+		PATH=/opt/anaconda/bin:$PATH
+		echo "Anaconda is already installed"
+	fi
 else
 	echo "ERROR: Anaconda installer is not found"
+fi
+
+# Jupyter
+
+# Git
+echo "Installing Git"
+apt-get install git -y > /dev/null
+
+# Reloading bash 
+echo "Reloading .bashrc"
+source /home/vagrant/.bashrc
+
+# Starting IPython Notebook
+echo "Trying to start IPython Notebook"
+notebook="/home/vagrant/ipython"
+if [ -d "$notebook" ]; then
+	ipython notebook --ip='*' --notebook-dir="$notebook" &
+else
+	echo "ERROR: IPython Notebook was not started."
+	echo "INFO: Try to create folder $notebook"
 fi
 
 echo "Finished provisioning."
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A collection of the different vagrant templates for personal usage.
 
 ### Collection of templates
 
+* [AnacondaVM](AnacondaVM)
 * [PredictionIO](PredictionIO)
     - [PredictionIO installation guide](https://docs.prediction.io/install/)
     - [Quick Start with Recommendation Engine Template](https://docs.prediction.io/templates/recommendation/quickstart/)
