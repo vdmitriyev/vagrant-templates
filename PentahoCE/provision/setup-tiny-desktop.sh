249a985764d26067ec123f58aec47a3c7a585ef3
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2018-11-23 16:46:26 +0100

    UPDATE: Vagrant and provision of the tiny desktop

--- PentahoCE/provision/setup-tiny-desktop.sh
+++ PentahoCE/provision/setup-tiny-desktop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Installing tiny and lightweight GUI desktop on Ubuntu (optional)"
+
+# install xfce and virtualbox additions
 sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+# Permit anyone to start the GUI
+sudo sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
