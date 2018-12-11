4ac8e086db54f823d359bffe46bc0660d1c84284
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2018-12-11 12:44:18 +0100

    UPDATE: A pgcli added to extra PostgreSQL provision configs.

--- PentahoCE/provision/setup-postgresql-extra-configs.sh
+++ PentahoCE/provision/setup-postgresql-extra-configs.sh
@@ -19,3 +19,7 @@ sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postg
 sed -i s/"host    all             all             127.0.0.1/32            md5"/"host    all             all             0.0.0.0/0            md5"/g /etc/postgresql/9.5/main/pg_hba.conf
 # revert back
 # sed -i s/"host    all             all             0.0.0.0/0            md5"/"host    all             all             127.0.0.1/32            md5"/g /etc/postgresql/9.5/main/pg_hba.conf
+
+
+# A REPL for Postgres
+pip install -U pgcli
